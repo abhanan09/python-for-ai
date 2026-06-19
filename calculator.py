fa3acc3187d5bdae8f0e52bf938f211f41652f0d
Abdul Hanan <hafizabdulhanan09@gmail.com>
2026-06-19 21:38:41 +0500

    A calculator

--- calculator.py
+++ calculator.py
@@ -1,19 +1,27 @@
-num1 = int(input("Enter first number: "))
-num2 = int(input("Enter second number: "))
+while True:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
 
-operation = input("Choose operation (+,-,*,/): ")
+    operation = input("Choose operation (+,-,*,/): ")
 
-if operation == "+":
-    result = num1 + num2
-elif operation == "-":
-    result = num1 - num2
-elif operation == "*":
-    result = num1 * num2
-elif operation == "/":
-    result = num1 / num2
-else:
-    result = "Invalid operation"
+    while operation not in ["+", "-", "*", "/"]:
+        operation = input("Invalid Choice. Choose operation ( +,-, *, / )")
 
-print("Result:", result)
+    if operation == "+":
+        result = num1 + num2
+    elif operation == "-":
+        result = num1 - num2
+    elif operation == "*":
+        result = num1 * num2
+    elif operation == "/":
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            result = "Error: Cannot divide by zero"
 
+    print("Result:", result)
 
+    again = input("Calculate Again? (yes/ no): ")
+    if again.lower() != "yes":
+        print("Goodbye!")
+        break
